a850201bac37e8a642c643ba128bc991e6ff7405
Dron2019 <mr.andrey@bigmir.net>
2021-12-02 16:03:30 +0200

    menu effect

--- src/assets/scripts/modules/animation/menu.js
+++ src/assets/scripts/modules/animation/menu.js
@@ -1,9 +1,29 @@
+import gsap from "gsap/all";
+
 export default function menuLinksEffect() {
+    const canvas = document.createElement('img');
+    canvas.setAttribute('data-menu-canvas', '');
     const images = [
         './assets/images/menu/Rectangle 3678.jpg',
         './assets/images/menu/Rectangle 3685.jpg',
         './assets/images/menu/Rectangle 3686.jpg',
         './assets/images/menu/Rectangle 4.jpg',
+        './assets/images/menu/Rectangle 4.jpg',
     ];
-    console.log(images);
+    const menu = document.querySelector('.menu');
+    menu.append(canvas);
+    const links = document.querySelectorAll('.menu-main__left-list:nth-child(-n+5)');
+    console.log(links);
+    links.forEach((link, index) => {
+        link.addEventListener('mouseenter',function(evt){
+            gsap.to(link, { x: 20 });
+            gsap.to(canvas, { xPercent: 0 });
+            
+            canvas.src = images[index];
+        });
+        link.addEventListener('mouseleave',function(evt){
+            gsap.to(link, { x: 0 })
+            gsap.to(canvas, { xPercent: 100 });
+        });
+    })
 }
