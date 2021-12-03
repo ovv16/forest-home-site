913cfd6e13e9b8b94e965c79b6ea6f3b2fb5789b
Dron2019 <mr.andrey@bigmir.net>
2021-12-03 09:02:56 +0200

    add curtain image in

--- src/assets/scripts/modules/animation/effect/paralax.js
+++ src/assets/scripts/modules/animation/effect/paralax.js
@@ -1,11 +1,27 @@
+import gsap from "gsap/all";
+
 export default function paralax(selector) {
     const paralaxImages = document.querySelectorAll(selector)
     paralaxImages.forEach((image) => {
         const wrap = document.createElement('div');
         wrap.style.overflow = 'hidden';
+        const curtain = document.createElement('div');
+        wrap.classList.add('image-with-curtain-in');
+        curtain.classList.add('curtain');
+        wrap.append(curtain);
         image.parentElement.prepend(wrap);
         gsap.set(image, { willChange: 'transform', scale: 1.1 });
         wrap.prepend(image);
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: image,
+                once: true,
+            }
+        })
+        .set(image, { autoAlpha: 0 })
+        .to(curtain, { scaleY: 1 })
+        .to(curtain, { scaleY: 0, transformOrigin: '50% 0%' })
+        .set(image, { autoAlpha: 1 }, '<')
 
         gsap.timeline({
             ease: 'none',
