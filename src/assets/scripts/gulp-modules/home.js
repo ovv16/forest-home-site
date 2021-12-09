76b166964d5978b63b4232f813356c5f05253b6b
unknown <liasav19@ukr.net>
2021-12-09 10:12:25 +0200

    edits2-0912

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   $('.section-2__right-slider').slick({
     speed: 1000,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    // slidesToScroll: 1,
     variableWidth: true,
     nextArrow: '<button type="button" class="slick-next"><svg width="11" height="16" viewBox="0 0 11 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L9 8L1 15" stroke="white" stroke-width="2"/></svg></button>',
     prevArrow: '<button type="button" class="slick-prev"><svg width="11" height="16" viewBox="0 0 11 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 1L2 8L10 15" stroke="white" stroke-width="2"/></svg></button>',
